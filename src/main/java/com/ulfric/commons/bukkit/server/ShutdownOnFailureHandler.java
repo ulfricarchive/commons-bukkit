06844b8890bf9192a482b8eced75b7a9451d2ddc
Adam Harrison <ad@mharrison.us>
2017-12-26 12:19:51 -0500

    updates

--- src/main/java/com/ulfric/commons/bukkit/server/ShutdownOnFailureHandler.java
+++ src/main/java/com/ulfric/commons/bukkit/server/ShutdownOnFailureHandler.java
@@ -2,23 +2,20 @@ package com.ulfric.commons.bukkit.server;
 
 import java.util.function.Consumer;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 
 import com.ulfric.dragoon.extension.inject.Inject;
+import com.ulfric.dragoon.logging.Log;
 
 public class ShutdownOnFailureHandler implements Consumer<Throwable> {
 
-	@Inject(optional = true)
-	private Logger logger;
+	@Inject
+	private Log logger;
 
 	@Override
 	public void accept(Throwable thrown) {
-		if (logger != null) {
-			logger.log(Level.SEVERE, "Caught fatal error", thrown);
-		}
-
+		logger.log(Level.SEVERE, "Caught fatal error", thrown);
 		Bukkit.shutdown();
 	}
 
