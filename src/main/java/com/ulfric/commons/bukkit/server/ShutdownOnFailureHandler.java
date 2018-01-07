5431d9beb10cdd0eae59178eb47ffe01388f4fa4
Adam Harrison <ad@mharrison.us>
2018-01-07 13:26:53 -0500

    logger fixes

--- src/main/java/com/ulfric/commons/bukkit/server/ShutdownOnFailureHandler.java
+++ src/main/java/com/ulfric/commons/bukkit/server/ShutdownOnFailureHandler.java
@@ -2,16 +2,16 @@ package com.ulfric.commons.bukkit.server;
 
 import java.util.function.Consumer;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 
 import com.ulfric.dragoon.extension.inject.Inject;
-import com.ulfric.dragoon.logging.Log;
 
 public class ShutdownOnFailureHandler implements Consumer<Throwable> {
 
 	@Inject
-	private Log logger;
+	private Logger logger;
 
 	@Override
 	public void accept(Throwable thrown) {
